3df91508295b324fb254956ce45de1328f6b9c0d
Andsu Duarte <andersonduarteg4@gmail.com>
2023-03-30 18:55:02 -0300

    teste

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import ProdutoView from "../views/ProdutoView.vue";
+import LoginView from "../views/LoginView.vue";
+import UsuarioView from "@/views/usuario/UsuarioView.vue";
+import UsuarioProdutos from "@/views/usuario/UsuarioProdutos.vue";
+import UsuarioVendas from "@/views/usuario/UsuarioVendas.vue";
+import UsuarioCompras from "@/views/usuario/UsuarioCompras.vue";
+import UsuarioEditar from "@/views/usuario/UsuarioEditar.vue";
 
 Vue.use(VueRouter);
 
@@ -10,12 +17,67 @@ const routes = [
     name: "home",
     component: HomeView,
   },
+  {
+    path: "/produto/:id",
+    name: "produto",
+    component: ProdutoView,
+    props: true,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
+  },
+  {
+    path: "/usuario",
+    component: UsuarioView,
+    meta: {
+      login: true,
+    },
+    children: [
+      {
+        path: "",
+        name: "usuario",
+        component: UsuarioProdutos,
+      },
+      {
+        path: "vendas",
+        name: "vendas",
+        component: UsuarioVendas,
+      },
+      {
+        path: "compras",
+        name: "compras",
+        component: UsuarioCompras,
+      },
+      {
+        path: "editar",
+        name: "usuario-editar",
+        component: UsuarioEditar,
+      },
+    ],
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+  scrollBehavior() {
+    return window.scrollTo({ top: 0, behavior: "smooth" });
+  },
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.login)) {
+    if (!window.localStorage.token) {
+      next("/login");
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <template>
-  <div class="home"></div>
+  <section>
+    <h1>🚀 Compre ou Venda 💸</h1>
+    <produtos-buscar-vue />
+    <produtos-lista-vue />
+  </section>
 </template>
 
 <script>
+import ProdutosBuscarVue from "@/components/ProdutosBuscar.vue";
+import ProdutosListaVue from "@/components/ProdutosLista.vue";
+
 export default {
   name: "HomeView",
+  components: {
+    ProdutosBuscarVue,
+    ProdutosListaVue,
+  },
 };
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
